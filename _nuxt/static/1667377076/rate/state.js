39b6d7f0ad5ff237d5b14e5fe91374f7579e0908
dpetrouk <dpetrouk@users.noreply.github.com>
2022-11-02 08:21:23 +0000

    deploy: 2461c97e284b5f76572a1586a0ea90f88bd073c4

--- _nuxt/static/1667377076/rate/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcurrency-converter\u002F_nuxt\u002Fstatic\u002F1667377076",layout:"default",error:a,state:{currencies:{}},serverRendered:true,routePath:"\u002Frate",config:{_app:{basePath:"\u002Fcurrency-converter\u002F",assetsPath:"\u002Fcurrency-converter\u002F_nuxt\u002F",cdnURL:a}}}}(null));
